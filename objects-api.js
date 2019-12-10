a0bceb9fb745414ad9a4f688786bb935b73197e9
ualnaji@cnm.edu <ualnaji@cnm.edu>
2019-12-09 19:56:31 -0700

    Completed Fundamentals Prework

---
+++ objects-api.js
@@ -0,0 +1,30 @@
+/*
+Objects, Interfaces, and API's
+ */
+
+/*
+let dog = {
+    name: "Steve",
+    color: "brown",
+    breed: "Pug",
+    size: "Small",
+    bark: function(typeOfBark) {
+        console.log("Bark!");
+    },
+};
+
+dog.bark();
+
+ */
+
+function x(y) {
+    y.num = y.num + 5;
+    console.log(y);
+}
+
+let y = {
+    name: "Tom",
+    num: 10,
+};
+x(y);
+console.log(y);
---
+++ variables-and-data.js
@@ -0,0 +1,58 @@
+/*
+ * Variables
+ */
+/*
+let wordData = "Word Data";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+
+/*
+ * Data Types (Objects)
+ */
+/*
+
+let obj = {
+    key1: "Value 1",
+    key2: 3,
+    boolean: true,
+    obj2: {
+        obj2key1: "value of inner object",
+    },
+};
+
+console.log(obj.obj2.obj2key1);
+
+/*
+ *Typing Systems
+ */
+
+/*
+
+let variable : string = "string";
+
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result);
+
+/*
+ *Operators
+ */
+
+
+let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2*4;
+let division =4/2;
+let mod = 5 % 2;
+
+let string =  "String 1 " + "String 2";
+
+console.log (string);
+
+
